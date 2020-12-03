a5349b86b36c8f7068256b42eaa7979891dd79f9
Andi Syafrianda <andi.syafrianda@aux.go-jek.com>
2020-12-03 09:25:47 +0700

    create user validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-// @route  GET api/users
-// @desc   Test route
+// @route  POST api/users
+// @desc   Register User
 // @access Public
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    body("name", "Name is required").not().isEmpty(),
+    body("email", "Please include a valid email").isEmail(),
+    body(
+      "password",
+      "Please enter a password with 6 or more character"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send("User route success");
+  }
+);
 
 module.exports = router;
