13697f39337be2585c3a5beea63d786fa647595e
Andi Syafrianda <andi.syafrianda@aux.go-jek.com>
2020-12-05 22:55:56 +0700

    Get Profile by ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -104,4 +104,36 @@ router.post(
   }
 );
 
+// @route  GET api/profile
+// @desc   get all profile
+// @access Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("error");
+  }
+});
+
+// @route  GET api/profile/user/:user_id
+// @desc   get  profile by id
+// @access Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) return res.status(400).json({ msg: "no user found" });
+    res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("error");
+  }
+});
+
 module.exports = router;
